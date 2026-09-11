d2800f5f4a062ae966c7c2de9977259982ee67b6
Sahel-LM <sahel.sm03@gmail.com>
2026-09-12 02:48:29 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-red-600">Assingment-05</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
