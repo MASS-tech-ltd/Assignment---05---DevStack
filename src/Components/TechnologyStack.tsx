bb2f894721e249b4e367c16c6e67fc5d6c27a7e2
Sahel-LM <sahel.sm03@gmail.com>
2026-09-13 06:02:44 +0600

    some responsive error fixed

--- src/Components/TechnologyStack.tsx
+++ src/Components/TechnologyStack.tsx
@@ -16,7 +16,7 @@ const TechnologyStack = ({ technologysPromise }: technologyStackPropsType) => {
         <div className="container mx-auto">
             <h1 className="ml-6 lg:ml-0 font-extrabold font-inter lg:text-4xl text-3xl text-slate-900">Explore the <span className="bg-linear-to-r from-orange-500 to-purple-600 bg-clip-text text-transparent">Technologies</span></h1>
             <p className="ml-6 lg:ml-0 font-jakarta font-normal lg:text-base text-sm mt-2 mb-6 lg:mb-10 text-slate-500">Pick one technology per category to build your ideal stack.</p>
-            <div className="grid grid-cols-1 lg:grid-cols-[3fr_1fr] gap-6">
+            <div className="mx-6 lg:mx-0 grid grid-cols-1 lg:grid-cols-[3fr_1fr] gap-6">
                 <TechnologyList
                     technologies={technologies}
                     addedTechnology={addedTechnology}
